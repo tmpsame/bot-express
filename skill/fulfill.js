3b50def4a0ececde4637f81235cb30b96564e1c1
nkjm <nkjm.kzk@gmail.com>
2017-02-23 16:09:09 +0900

    dev

---
+++ skill/fulfill.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let Line = require("../service/line");
+let Promise = require("bluebird");
+
+module.exports = class SkillFulfill {
+
+    constructor() {
+    }
+
+    finish(message_platform_type, message_platform, bot_event, conversation){
+        switch(message_platform_type){
+            case "line":
+                let messages = [{
+                    type: "text",
+                    text: conversation.intent.fulfillment.speech
+                }];
+                let
+                return message_platform.reply_message(bot_event.replyToken, messages);
+            break;
+            default:
+                return Promise.reject(`Unsupported message platform type: ${message_platform_type}`);
+            break;
+        }
+    }
+};
