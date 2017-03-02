48f40f18884dcb35b8fdd8845103701b8482b21e
nkjm <nkjm.kzk@gmail.com>
2017-03-02 00:26:45 -0800

    added facebook support

--- sample_app.js
+++ sample_app.js
@@ -16,28 +16,24 @@ app.listen(process.env.PORT || 5000, () => {
     console.log(`server is running...`);
 });
 
-/*
-** Router Configuration
-*/
-
 // For LINE
-let line_option = {
+app.use('/webhook/line', bot_express({
     message_platform_type: "line",
     line_channel_id: process.env.LINE_CHANNEL_ID,
     line_channel_secret: process.env.LINE_CHANNEL_SECRET,
     line_channel_access_token: process.env.LINE_CHANNEL_ACCESS_TOKEN,
     apiai_client_access_token: process.env.APIAI_CLIENT_ACCESS_TOKEN,
     default_skill: 'apologize'
-}
-app.use('/webhook/line', bot_express(line_option));
+}));
 
 // For Facebook
-let facebook_option = {
+/*
+app.use('/webhook/facebook', bot_express({
     message_platform_type: "facebook",
     facebook_page_access_token: process.env.FACEBOOK_PAGE_ACCESS_TOKEN,
     apiai_client_access_token: process.env.APIAI_CLIENT_ACCESS_TOKEN,
     default_skill: 'apologize'
-}
-app.use('/webhook/facebook', bot_express(facebook_option));
+}));
+*/
 
 module.exports = app;
