399fda89457d57331413b9cb6365a1039730cf3f
nkjm <nkjm.kzk@gmail.com>
2017-03-02 01:59:55 -0800

    added facebook support

--- sample_app.js
+++ sample_app.js
@@ -5,7 +5,7 @@
 */
 let express = require("express");
 let logger = require('morgan');
-let Bot_express = require("./module/index2.js");
+let bot_express = require("./index.js");
 
 /*
 ** Middleware Configuration
@@ -18,15 +18,14 @@ app.listen(process.env.PORT || 5000, () => {
 });
 
 // For LINE
-let b = new Bot_express({
+app.use('/webhook/line', bot_express({
     message_platform_type: "line",
     line_channel_id: process.env.LINE_CHANNEL_ID,
     line_channel_secret: process.env.LINE_CHANNEL_SECRET,
     line_channel_access_token: process.env.LINE_CHANNEL_ACCESS_TOKEN,
     apiai_client_access_token: process.env.APIAI_CLIENT_ACCESS_TOKEN,
     default_skill: 'apologize'
-});
-app.use('/webhook/line', b.webhook);
+}));
 
 // For Facebook
 /*
