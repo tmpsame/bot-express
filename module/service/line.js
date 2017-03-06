cf518851a57e43fa65cf2d39d28eb332d121e38b
nkjm <nkjm.kzk@gmail.com>
2017-03-06 13:27:31 +0900

    changed sample skill to selectable question

--- module/service/line.js
+++ module/service/line.js
@@ -63,6 +63,7 @@ module.exports = class ServiceLine {
                     return reject(error);
                 }
                 if (response.statusCode != 200){
+                    debug(body.message);
                     return reject(body.message || "Failed to reply.");
                 }
                 resolve();
