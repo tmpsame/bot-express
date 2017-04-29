67d77913420a28acf6f7c7109f90de2c4ee96891
Kazuki Nakajima <nkjm@nkjm-macbook-air.local>
2017-04-29 09:08:57 +0900

    test

--- sample_app.js
+++ sample_app.js
@@ -17,12 +17,13 @@ app.listen(process.env.PORT || 5000, () => {
 });
 
 app.use('/webhook', bot_express({
+    apiai_client_access_token: process.env.APIAI_CLIENT_ACCESS_TOKEN,
     line_channel_id: process.env.LINE_CHANNEL_ID,
     line_channel_secret: process.env.LINE_CHANNEL_SECRET,
     line_channel_access_token: process.env.LINE_CHANNEL_ACCESS_TOKEN,
     facebook_app_secret: process.env.FACEBOOK_APP_SECRET,
     facebook_page_access_token: process.env.FACEBOOK_PAGE_ACCESS_TOKEN,
-    apiai_client_access_token: process.env.APIAI_CLIENT_ACCESS_TOKEN,
+    default_skill: "faq",
     beacon_skill: {
         enter: "survey",
         leave: "bye"
