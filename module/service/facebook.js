511f42f7900ab2a1cada0a63f3087fbf226e0840
Kazuki Nakajima <nkjm@nkjm-macbook-air.local>
2017-05-08 17:13:38 +0900

    try adding delay to facebook.send()

--- module/service/facebook.js
+++ module/service/facebook.js
@@ -54,8 +54,8 @@ module.exports = class ServiceFacebook {
                         }
                         resolve();
                     });
-                })), 3000);
-            }
+                }));
+            }, 3000);
         }
         return Promise.all(all_sent).then(
             (response) => {
