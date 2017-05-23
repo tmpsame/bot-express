60daf3b2b5abf6d29e39da3334e1865cc7607008
Kazuki Nakajima <nkjm@nkjm-macbook-air.local>
2017-05-23 15:45:41 +0900

    fix postinstall script

--- script/postinstall.js
+++ script/postinstall.js
@@ -5,6 +5,7 @@ var readline = require("readline");
 var os = require("os");
 var skill_dir = "../../skill";
 var index_script = "../../index.js";
+var filecopy = require("filecopy");
 
 if (!process.env.TRAVIS && process.env.NODE_ENV != "test" && process.env.NODE_ENV != "production"){
 
@@ -19,10 +20,13 @@ if (!process.env.TRAVIS && process.env.NODE_ENV != "test" && process.env.NODE_EN
 
                     rl.question('May I create skill directory and index.js for you? (y/n): ', function(answer){
                         if (answer == "y"){
-                            create_skill_dir();
-                            create_indexjs();
+                            create_skill_dir(skill_dir);
+                            create_indexjs(index_script).then(
+                                function(response){
+                                    rl.close();
+                                }
+                            )
                         }
-                        rl.close();
                     });
                 }
             });
@@ -30,42 +34,12 @@ if (!process.env.TRAVIS && process.env.NODE_ENV != "test" && process.env.NODE_EN
     });
 }
 
-function create_skill_dir(){
+function create_skill_dir(skill_dir){
     console.log("Creating skill directory for you...");
     fs.mkdir(skill_dir);
 }
 
-function create_indexjs(){
+function create_indexjs(index_script){
     console.log("Creating index.js for you...");
-    fs.writeFile(index_script, `
-"use strict";
-
-/*
-** Import Packages
-*/
-let app = require("express")();
-let bot_express = require("bot-express");
-
-/*
-** Middleware Configuration
-*/
-app.listen(process.env.PORT || 5000, () => {
-    console.log("server is running...");
-});
-
-/*
-** Mount bot-express
-*/
-app.use("/webhook", bot_express({
-    apiai_client_access_token: process.env.APIAI_CLIENT_ACCESS_TOKEN,
-    line_channel_id: process.env.LINE_CHANNEL_ID,
-    line_channel_secret: process.env.LINE_CHANNEL_SECRET,
-    line_channel_access_token: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-    facebook_app_secret: process.env.FACEBOOK_APP_SECRET,
-    facebook_page_access_token: [
-        {page_id: process.env.FACEBOOK_PAGE_ID, page_access_token: process.env.FACEBOOK_PAGE_ACCESS_TOKEN}
-    ]
-}));
-
-module.exports = app;`);
+    return filecopy("./script/index.js", index_script);
 }
