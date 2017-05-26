16d031dedd7cf08f96a1f945f77c4d5f67b8a842
Kazuki Nakajima <nkjm@nkjm-macbook-air.local>
2017-05-26 13:57:51 +0900

    fix bug

--- sample_skill/simple-response.js
+++ sample_skill/simple-response.js
@@ -9,22 +9,8 @@ module.exports = class SkillSimpleResponse {
     finish(bot, bot_event, context, resolve, reject){
         debug(`Going to reply "${context.intent.fulfillment.speech}".`);
         let messages = [{
-            type: "template",
-            altText: "hoge",
-            template: {
-                text: "hoge",
-                actions: [{
-                    type: "text",
-                    label: "hoge1",
-                    text: "hoge1"
-                }]
-            }
-        }]
-        /*
-        let messages = [{
             text: context.intent.fulfillment.speech
         }];
-        */
         return bot.reply(messages).then(
             (response) => {
                 return resolve(response);
