05827c81b9edc05d0b052b44e5cd433ec35efff6
Kazuki Nakajima <nkjm@dhcp-tokyo-twvpn-1a-vpnpool-10-191-4-170.vpn.oracle.com>
2017-07-21 10:41:44 +0900

    fix

--- typedef/global.js
+++ typedef/global.js
@@ -1,7 +1,7 @@
 /**
 Object which contains context information.
 @typedef {Object} context
-@prop {Array.<Object>} to_confirm
-@prop {Sting} confirming
-@prop {Object} confirmed
+@prop {Array.<Object>} to_confirm - Array of parameters to confirm.
+@prop {Sting} confirming - Parameter name which Bot is now confirming.
+@prop {Object} confirmed - Object which contains confirmed value of the parameters as properties. If you want to retrieve confirmed value of "date" parameter, access confirmed.date.
 */
